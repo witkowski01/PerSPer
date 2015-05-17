8b07a2c8b2d87e6978d484f8d61c7c417798bd01
witkowski01 <witkowski01@wp.pl>
2015-05-17 13:36:23 +0200

    dalsza część wszystko nie przeszło na początku

---
+++ PerSPer/Server/MyConfig.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XSockets.Core.Configuration;
+
+namespace Server
+{
+    class MyConfig : ConfigurationSetting
+    {
+        public MyConfig()
+            : base("ws://localhost:8080")
+        {
+
+        }
+    }
+}
